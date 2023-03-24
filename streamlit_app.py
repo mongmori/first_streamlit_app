dd6ab45458d64191d66e424ccfc8bab166d62948
mongmori <42838525+mongmori@users.noreply.github.com>
2023-03-24 17:51:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load lost contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
